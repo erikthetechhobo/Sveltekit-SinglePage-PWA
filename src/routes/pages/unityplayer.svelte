4c0a6130578d53fa0a56db90e9fb40122c8c8053
erikthetechhobo@gmail.com <erikthetechhobo@gmail.com>
2022-03-04 22:00:28 -0600

    unity player working

--- src/routes/pages/unityplayer.svelte
+++ src/routes/pages/unityplayer.svelte
@@ -1,34 +1,49 @@
 <script>
     import {onMount} from 'svelte';
     function createCanvas() {
-        let unityCanvas = document.createElement('canvas');
-        unityCanvas.id = 'unity-canvas';
-        unityCanvas.style.width = '200px';
-        unityCanvas.style.height = '300px';
-        unityCanvas.style.backgroundColor = '#231F20';
-        document.getElementById('ThisPage').appendChild(unityCanvas);
+        return new Promise(resolve =>{
+            let unityCanvas = document.createElement('canvas');
+            unityCanvas.id = 'unity-canvas';
+            unityCanvas.style.width = '200px';
+            unityCanvas.style.height = '300px';
+            unityCanvas.style.backgroundColor = '#231F20';
+            document.getElementById('ThisPage').appendChild(unityCanvas);
+            resolve('canvas created');
+        });
     }
     function createScript() {
-        let unityLoaderScript = document.createElement('script');
-        unityLoaderScript.src = '/Build/build1.loader.js';
-        document.getElementById('App').appendChild(unityLoaderScript);
+        return new Promise(resolve => {
+            let unityLoaderScript = document.createElement('script');
+            unityLoaderScript.src = '/Build/build1.loader.js';
+            unityLoaderScript.id = 'myUnityLoaderScript';
+            document.getElementById('App').appendChild(unityLoaderScript);
+            resolve('script created');
+        })
     }
     function loadGame() {
-        createUnityInstance(document.getElementById("unity-canvas"), {
-            dataUrl: "/Build/build1.data",
-            frameworkUrl: "/Build/build1.framework.js",
-            codeUrl: "/Build/build1.wasm",
-            streamingAssetsUrl: "StreamingAssets",
-            companyName: "DefaultCompany",
-            productName: "htmlBuildTest",
-            productVersion: "0.1",
-            // matchWebGLToCanvasSize: false, // Uncomment this to separately control WebGL canvas render size and DOM element size.
-            // devicePixelRatio: 1, // Uncomment this to override low DPI rendering on high DPI displays.
+        return new Promise(resolve =>{
+            createUnityInstance(document.getElementById("unity-canvas"), {
+                dataUrl: "/Build/build1.data",
+                frameworkUrl: "/Build/build1.framework.js",
+                codeUrl: "/Build/build1.wasm",
+                streamingAssetsUrl: "StreamingAssets",
+                companyName: "DefaultCompany",
+                productName: "htmlBuildTest",
+                productVersion: "0.1",
+                // matchWebGLToCanvasSize: false, // Uncomment this to separately control WebGL canvas render size and DOM element size.
+                // devicePixelRatio: 1, // Uncomment this to override low DPI rendering on high DPI displays.
+            })
+            resolve('game loaded');
         })
     }
     onMount(async () => {
-        createCanvas();
-        //createScript();    
+        await createCanvas();
+        await createScript();
+        await loadGame();
+
+        window.onbeforeunload = function () {
+            unityInstance.Quit();
+        }
     });
 </script>
 <div id="ThisPage">
